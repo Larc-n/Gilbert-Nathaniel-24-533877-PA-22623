19a00e8a5d881d0ad6b427ed4585572a5298cce3
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-09-17 15:03:58 +0700

    Update code.cpp

--- Pertemuan 4/Soal 2/code.cpp
+++ Pertemuan 4/Soal 2/code.cpp
@@ -7,7 +7,5 @@ int main()
     int input;cout<<"Input: "; cin>>input;
 
     for(int loop=1; loop<11; loop++)
-    {
-        printf("%i x %i = %i\n", input, loop, input*loop);
-    }
+    {printf("%i x %i = %i\n", input, loop, input*loop);}
 }
