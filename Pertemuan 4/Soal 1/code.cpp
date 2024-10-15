572a9771491dfdce4c8e1501074d29188467597e
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-10-15 17:20:51 +0700

    Update code.cpp

--- Pertemuan 4/Soal 1/code.cpp
+++ Pertemuan 4/Soal 1/code.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-    int input;
-    do
-    {cout<<"Input: "; cin>>input;}
-    while(input<0);
+    int test[6];
+    test[0]=5;
+    test[1]=0;
+    test[2]=123;
+    test[3]=7;
+    test[4]=68;
+    test[5]=1;
 
-    int hasil=0;
-    for(int loop=0; loop<input+1; loop++)
-    {hasil+=loop;}
+    int expected[6];
+    expected[0]=15;
+    expected[1]=0;
+    expected[2]=7626;
+    expected[3]=28;
+    expected[4]=2346;
+    expected[5]=1;
 
-    cout<<hasil<< endl;
+
+    for(int loop=0; loop<6; loop++)
+    {
+        int input=test[loop];
+        int hasil=0;
+        for(int loop=0; loop<input+1; loop++)
+        {hasil+=loop;}
+
+        if(hasil==expected[loop]){cout<<"Test "<<loop+1<< " sesuai, ";}
+        cout<<hasil<< endl;
+    }
+
+
+
 }
