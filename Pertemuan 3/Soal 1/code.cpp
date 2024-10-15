c5e7efdd05451caedc7b11de6d20d7889e65caa1
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-10-15 17:42:15 +0700

    Update code.cpp

--- Pertemuan 3/Soal 1/code.cpp
+++ Pertemuan 3/Soal 1/code.cpp
@@ -4,23 +4,36 @@
 using namespace std;
 int main ()
 {
-    float a; cout<< "Input a: "; cin>> a;
-    float b; cout<< "Input b: "; cin>> b;
-    float c; cout<< "Input c: "; cin>> c;
-    float D= b*b - 4*a*c;
+    int data[3][3]=
+    {
+        { 1,13, 2},
+        { 1, 2, 1},
+        {10, 1, 4}
+    };
 
-    if(D>0)
+    for(int loop=0; loop<3; loop++)
     {
-        float x1= (-b +sqrt(D))/(2*a);
-        float x2= (-b -sqrt(D))/(2*a);
-        printf("x1: %f and x2: %f\n", x1, x2);
+        float a=data[loop][0];
+        float b=data[loop][1];
+        float c=data[loop][2];
+        float D= b*b - 4*a*c;
+
+        printf("Test %i\n", loop+1);
+        printf("a=%f b=%f c=%f\n", a,b,c);
+
+        if(D>0)
+        {
+            float x1= (-b +sqrt(D))/(2*a);
+            float x2= (-b -sqrt(D))/(2*a);
+            printf("x1: %f and x2: %f\n\n", x1, x2);
+        }
+        else if(D==0)
+        {
+            float x= -b/(2*a);
+            printf("x: %f\n\n", x);
+        }
+        else
+        {printf("x1 dan x2 bersifat imaginer\n\n");}
     }
-    else if(D==0)
-    {
-        float x= -b/(2*a);
-        printf("x: %f\n", x);
-    }
-    else
-    {printf("x1 dan x2 bersifat imaginer\n");}
-    return 0;
+
 }
