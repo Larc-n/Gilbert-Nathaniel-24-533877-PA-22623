cf3602c9f9ede6039b4f69ed36b74cba8fa583e5
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-10-15 14:55:27 +0700

    Update Test1.cpp

--- Pertemuan 6/Test/Test1.cpp
+++ Pertemuan 6/Test/Test1.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+typedef struct
+{
+    string name;
+    int price;
+    int kode;
+    int quantity;
+}array;
+
+int main()
+{
+    array stuff[3];
+    
+    stuff[0].name = "Tupperware";
+    stuff[0].price = 125;
+    stuff[0].kode = 101;
+    stuff[0].quantity = 50;
+
+    stuff[1].name = "Thermos";
+    stuff[1].price = 250;
+    stuff[1].kode = 102;
+    stuff[1].quantity = 30;
+
+    stuff[2].name = "Lunch Box";
+    stuff[2].price = 175;
+    stuff[2].kode = 103;
+    stuff[2].quantity = 45;
+
+    stuff[3].name = "Water Bottle";
+    stuff[3].price = 90;
+    stuff[3].kode = 104;
+    stuff[3].quantity = 60;
+    
+    int total=0; float mean=0;
+
+    for(int loop=0; loop<n; loop++)
+    {
+        total+=stuff[loop].quantity;
+        mean+= (float)stuff[loop].price * (float)stuff[loop].quantity;
+    }
+
+    cout<<"\nJumlah semua barang: "<<total<< "\nHarga rata-rata barang: "<< mean/(float)total<< endl;
+}
