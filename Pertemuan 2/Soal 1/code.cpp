ef99c73bca9376f24c2e41bf07b1a74393ded386
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-11-04 22:48:43 +0700

    Update code.cpp

--- Pertemuan 2/Soal 1/code.cpp
+++ Pertemuan 2/Soal 1/code.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <cmath>
+#include <stdio.h>
 using namespace std;
 
 int main ()
 {
-    float a; cout<<"A: "; cin>> a;
-    float b; cout<<"B: "; cin>> b;
-    float c; cout<<"C: "; cin>> c;
 
-    float D= b*b - 4*a*c;
+    int data[3][3]=
+    {
+        { 1,13, 2},
+        { 1, 2, 1},
+        {10, 1, 4}
+    };
 
-    if(D>0)
+    for(int loop=0; loop<3; loop++)
     {
-        float x1= (-b+ sqrt(D))/2*a;
-        float x2= (-b- sqrt(D))/2*a;
+        float a=data[loop][0];
+        float b=data[loop][1];
+        float c=data[loop][2];
 
-        cout<< "x1: "<< x1 << endl;
-        cout<< "x2: "<< x2 << endl;
-    }
-    else if(D==0)
-    {
-        float x= -b/(2*a);
-        cout<< "x: "<< x<< endl;
-    }
-    else
-    {
-        cout<<"There are no real roots\n";
+        float D= b*b - 4*a*c;
+
+        if(D>0)
+        {
+            float x1= (-b+ sqrt(D))/2*a;
+            float x2= (-b- sqrt(D))/2*a;
+            printf("For a=%.2f, b=%.2f, c=%.2f\nx1= %f\nx2= %f\n\n", a, b, c, x1, x2);
+        }
+        else if(D==0)
+        {
+            float x= -b/(2*a);
+            printf("For a=%.2f, b=%.2f, c=%.2f\nx: %f\n\n", a, b, c, x);
+        }
+        else
+        {
+            printf("For a=%.2f, b=%.2f, c=%.2f\nThere are no real roots\n\n", a, b, c);
+        }
+
     }
 }
