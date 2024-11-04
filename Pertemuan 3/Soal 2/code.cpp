97c3d40cd2a45b43fa45002eccf361d33f210b85
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-11-04 22:31:20 +0700

    Update code.cpp

--- Pertemuan 3/Soal 2/code.cpp
+++ Pertemuan 3/Soal 2/code.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
-    int n[3];
-    cout<<"Input a: "; cin>> n[0];
-    cout<<"Input b: "; cin>> n[1];
-    cout<<"Input c: "; cin>> n[2];
-
-    int biggest=n[0];
-    for(int loop=1; loop<3; loop++)
+    int data[]={0,3,4, 6,21,9, 10,90,64};
+    for(int a=0; a<7; a+=3)
     {
-        if(biggest<n[loop])
-        {biggest=n[loop];}
+        int n[3];
+        n[0]= data[a];
+        n[1]= data[a+1];
+        n[2]= data[a+2];
+
+        int biggest=n[0];
+        for(int loop=1; loop<3; loop++)
+        {
+            if(biggest<n[loop])
+            {biggest=n[loop];}
+        }
+        printf("For %i, %i, and %i. Biggest = %i\n", data[a], data[a+1], data[a+2], biggest);
     }
-    cout<< biggest<< endl;
-    return 0;
 }
