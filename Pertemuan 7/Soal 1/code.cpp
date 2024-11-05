0bfcb06412932b237a345cd9353bf07f752b3f0e
LarcN <109009731+Larc-n@users.noreply.github.com>
2024-11-05 15:12:56 +0700

    Update code.cpp

--- Pertemuan 7/Soal 1/code.cpp
+++ Pertemuan 7/Soal 1/code.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int fibo(int n);
 int main()
 {
-    int n; cout<<"Input: "; cin>>n;
-    int output= fibo(n);
-    cout<< output<< endl;
+    int data[]={10, 15, 20};
+    for(int loop=0; loop<3; loop++)
+    {
+        int n=data[loop];
+        int output= fibo(n);
+        printf("Input: %i\nOutput:%i\n\n", data[loop], fibo(n));
+    }
 }
 
 int fibo(int n)
